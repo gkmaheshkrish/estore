e2c907b2fea9adaa081ba35478148b98be161377
gkmaheshkrish <gkmaheshkrish@gmail.com>
2019-06-02 03:12:32 +0530

    PRODUCT PAGE

--- src/components/Productlist.js
+++ src/components/Productlist.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
-import { storeProducts } from "../data";
 import styled from "styled-components";
 import { ProductConsumer } from "../context";
 export default class ProductList extends Component {
-  state = {
-    products: storeProducts
-  };
+ 
   render() {
     return (
       <React.Fragment>
