851b79dbae9521045784b5b0481f428b03d10f3d
gkmaheshkrish <gkmaheshkrish@gmail.com>
2019-06-02 02:52:43 +0530

    TESTED WITH VALUE PASSING IN WITH CONTEXT API SUCCESS

--- src/components/Productlist.js
+++ src/components/Productlist.js
@@ -11,15 +11,19 @@ export default class ProductList extends Component {
   render() {
     return (
       <React.Fragment>
-       <div className="py-5">
+       <div className="py-5"> 
           <div className="container">
             <Title name="our" title="products" />
             <div className="row">
               <ProductConsumer>
                 {
-                    hello =>{
-                        return <h1>{hello}</h1>;
-                            }
+                    value =>{
+                      return value.products.map(product =>{
+                            return <Product key={product.id} product={product}>
+
+                            </Product>
+                      })  
+                     }
                 }
               </ProductConsumer>
             </div>
--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {storeProducts,detailProduct} from './data';
 
 
 const ProductContext =React.createContext();
@@ -7,10 +8,25 @@ const ProductContext =React.createContext();
 //consumer
 
 class ProductProvider  extends Component {
+    state={
+        products:storeProducts,
+        detailProduct:detailProduct
+    }
+    handleDetail = () =>{
+        console.log('hello from detail');
+    }
+    addToCart = () =>{
+        console.log('hello from add to cart');
+    }
     render() {
         return (
             <div>
-                <ProductContext.Provider value="hello from context">
+                <ProductContext.Provider value={{
+                    ...this.state,
+                    handleDetail:this.handleDetail,
+                    addToCart:this.addToCart
+
+                }}>
                     {this.props.children}
                 </ProductContext.Provider>
                 
